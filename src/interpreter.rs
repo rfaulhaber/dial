d27e0c322a47366e1a3710442a7e01abbaf41b00
Ryan Faulhaber <faulhaberryan@gmail.com>
2019-03-14 20:19:42 -0400

    worked on parsing still

--- src/interpreter.rs
+++ src/interpreter.rs
@@ -1,11 +1,12 @@
 use super::parser::{DialParser, Rule};
 use pest::error;
-use pest::iterators::Pair;
+use pest::iterators::{Pair, Pairs};
 use pest::Parser;
+use std::iter::FromIterator;
 
 #[derive(Debug)]
 pub enum DialValue {
-	Integer(u64),
+	Integer(i64),
 	Float(f64),
 	String(String),
 	Nil,
@@ -23,27 +24,43 @@ pub fn eval_line(input: &str) -> Result<DialValue, error::Error<Rule>> {
 
 // TODO return DialType
 fn eval(input: &str, rule: Rule) -> Result<DialValue, error::Error<Rule>> {
-	let parse_result = DialParser::parse(rule, input)?;
+	let pair = DialParser::parse(rule, input)?.next().unwrap();
 
-	let stack: Vec<DialValue> = Vec::new();
+	let mut stack: Vec<Rule> = Vec::new();
 
-	for pair in parse_result {
-		println!("pair: {:?}", pair);
-		println!("as rule: {:?}", pair.as_rule());
-		match pair.as_rule() {
-			Rule::COMMENT | Rule::nil => return Ok(DialValue::Nil),
-			_ => unimplemented!(),
-		}
+	eprintln!("pair: {:?}", pair);
+	eprintln!("as rule: {:?}", pair.as_rule());
+	let val = match pair.as_rule() {
+		Rule::add => eval_add(pair.into_inner()),
+		Rule::subtract => eval_sub(pair.into_inner()),
+		Rule::float => DialValue::Float(pair.as_str().parse().unwrap()),
+		Rule::int => DialValue::Integer(pair.as_str().parse().unwrap()),
+		Rule::COMMENT | Rule::nil => DialValue::Nil,
+		_ => unimplemented!(),
+	};
+
+	Ok(val)
+}
+
+fn eval_expr(pair: Pair<Rule>) -> DialValue {
+	match pair.as_rule() {
+		Rule::add => eval_add(pair.into_inner()),
+		_ => unimplemented!(),
 	}
+}
 
+fn eval_add(pairs: Pairs<Rule>) -> DialValue {
 	unimplemented!();
 }
 
-// fn parse_value(pair: Pair<Rule>) -> DialValue {
+fn eval_sub(pairs: Pairs<Rule>) -> DialValue {
+	unimplemented!();
+}
 
-// 	// match pair.as_rule() {
-// 	// 	Rule::expr => {}
-// 	// 	Rule::number => {}
-// 	// 	_ => unreachable!(),
-// 	// }
-// }
+fn eval_mult(pairs: Pairs<Rule>) -> DialValue {
+	unimplemented!();
+}
+
+fn eval_div(pairs: Pairs<Rule>) -> DialValue {
+	unimplemented!();
+}
