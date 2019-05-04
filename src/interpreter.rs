e5ba12cc2c7df2fa37dbf5d1580d74136686263e
Ryan Faulhaber <faulhaberryan@gmail.com>
2019-05-04 16:34:58 -0400

    sexpr take 3

--- src/interpreter.rs
+++ src/interpreter.rs
@@ -21,72 +21,71 @@ impl Interpreter {
         }
     }
 
-    pub fn eval(&self, expr: Sexpr) -> EvalResult {
-        let mut stack = Vec::new();
-        let mut tmp_stack = Vec::new();
-        let mut result_stack = Vec::new();
+    // pub fn eval(&self, expr: Sexpr) -> EvalResult {
+    //     let mut stack = Vec::new();
+    //     let mut tmp_stack = Vec::new();
+    //     let mut result_stack = Vec::new();
 
-        for sexpr in expr.into_iter() {
-            println!("sexpr: {:?}", sexpr);
-            match sexpr {
-                Sexpr::Nil => {
-                    println!("nil found, unwinding stack");
-                    for sexpr in stack.pop() {
-                        println!("element in stack: {:?}", sexpr);
-                        match sexpr {
-                            Sexpr::Symbol(s) => {
-                                match (get_builtin(s))(tmp_stack.clone()) {
-                                    Ok(val) => result_stack.push(val),
-                                    Err(e) => {
-                                        return Err(e);
-                                    }
-                                }
+    //     for sexpr in expr.into_iter() {
+    //         match sexpr {
+    //             Sexpr::Nil => {
+    //                 println!("nil found, unwinding stack");
+    //                 for sexpr in stack.pop() {
+    //                     println!("element in stack: {:?}", sexpr);
+    //                     match sexpr {
+    //                         Sexpr::Symbol(s) => {
+    //                             match (get_builtin(s))(tmp_stack.clone()) {
+    //                                 Ok(val) => result_stack.push(val),
+    //                                 Err(e) => {
+    //                                     return Err(e);
+    //                                 }
+    //                             }
 
-                                tmp_stack.clear();
+    //                             tmp_stack.clear();
 
-                                // pass op, vec into handler function
-                                break;
-                            }
-                            Sexpr::Integer(int) => tmp_stack.push(DialValue::Integer(int)),
-                            Sexpr::Float(fl) => tmp_stack.push(DialValue::Float(fl)),
-                            Sexpr::String(s) => tmp_stack.push(DialValue::String(s)),
-                            Sexpr::Boolean(b) => tmp_stack.push(DialValue::Boolean(b)),
-                            Sexpr::Identifier(id) => {
-                                let result = self.get_symbol(id);
+    //                             // pass op, vec into handler function
+    //                             break;
+    //                         }
+    //                         Sexpr::Integer(int) => tmp_stack.push(DialValue::Integer(int)),
+    //                         Sexpr::Float(fl) => tmp_stack.push(DialValue::Float(fl)),
+    //                         Sexpr::String(s) => tmp_stack.push(DialValue::String(s)),
+    //                         Sexpr::Boolean(b) => tmp_stack.push(DialValue::Boolean(b)),
+    //                         Sexpr::Identifier(id) => {
+    //                             let result = self.get_symbol(id);
 
-                                match result {
-                                    Some(result) => tmp_stack.push(result),
-                                    None => {
-                                        return Err("variable not defined");
-                                    }
-                                }
-                            }
-                            _ => unreachable!(),
-                        }
-                    }
-                }
+    //                             match result {
+    //                                 Some(result) => tmp_stack.push(result),
+    //                                 None => {
+    //                                     return Err("variable not defined");
+    //                                 }
+    //                             }
+    //                         }
+    //                         _ => unreachable!(),
+    //                     }
+    //                 }
+    //             }
 
-                _ => stack.push(sexpr),
-            }
-        }
+    //             _ => stack.push(sexpr),
+    //         }
+    //     }
 
-        Ok(result_stack[0].clone())
-    }
+    //     Ok(result_stack[0].clone())
+    // }
 
     fn get_symbol(&self, symbol: String) -> Option<DialValue> {
         self.env.borrow().get(&symbol)
     }
 }
 
-fn sexpr_to_value(expr: Sexpr) -> DialValue {
-    match expr {
-        Sexpr::Integer(int) => DialValue::Integer(int),
-        Sexpr::Float(fl) => DialValue::Float(fl),
-        Sexpr::Boolean(b) => DialValue::Boolean(b),
-        Sexpr::String(s) => DialValue::String(s),
-        _ => unreachable!(),
-    }
-}
+// fn sexpr_to_value(expr: Sexpr) -> DialValue {
+//     match expr {
+//         Sexpr::Integer(int) => DialValue::Integer(int),
+//         Sexpr::Float(fl) => DialValue::Float(fl),
+//         Sexpr::Boolean(b) => DialValue::Boolean(b),
+//         Sexpr::String(s) => DialValue::String(s),
+//         _ => unreachable!(),
+//     }
+// }
 
 fn get_builtin(symbol: String) -> BuiltinFunc {
     match symbol.as_str() {
@@ -96,20 +95,20 @@ fn get_builtin(symbol: String) -> BuiltinFunc {
     }
 }
 
-#[cfg(test)]
-mod interpreter_test {
-    use super::*;
-    use pest::Parser;
+// #[cfg(test)]
+// mod interpreter_test {
+//     use super::*;
+//     use pest::Parser;
 
-    #[test]
-    fn test_function_call() {
-        let mut parsed = DialParser::parse(Rule::list, "(* 2 (+ 3 4 5))").unwrap();
-        let ast = Sexpr::from_pair(parsed.next().unwrap());
+//     #[test]
+//     fn test_function_call() {
+//         let mut parsed = DialParser::parse(Rule::list, "(* 2 (+ 3 4 5))").unwrap();
+//         let ast = Sexpr::from_pair(parsed.next().unwrap());
 
-        let int = Interpreter::new();
-        let result = int.eval(ast);
+//         let int = Interpreter::new();
+//         let result = int.eval(ast);
 
-        assert!(result.is_ok());
-        assert_eq!(DialValue::Integer(24), result.unwrap());
-    }
-}
+//         assert!(result.is_ok());
+//         assert_eq!(DialValue::Integer(24), result.unwrap());
+//     }
+// }
--- src/parser.rs
+++ src/parser.rs
@@ -1,4 +1,4 @@
-use pest::iterators::Pair;
+use pest::iterators::{Pair, Pairs};
 use std::collections::VecDeque;
 
 #[derive(Parser)]
@@ -9,6 +9,7 @@ pub struct DialParser;
 
 pub struct ParseError;
 
+#[derive(Debug, PartialEq, Clone)]
 pub enum Atom {
 	Integer(i64),
 	Float(f64),
@@ -19,11 +20,6 @@ pub enum Atom {
 	Nil,
 }
 
-struct ConsCell {
-	car: Box<Sexpr>,
-	cdr: Box<Sexpr>,
-}
-
 // intermediate representation of expressions
 #[derive(Debug, PartialEq, Clone)]
 pub enum Sexpr {
@@ -32,44 +28,31 @@ pub enum Sexpr {
 	// (+ 1 2) => (+ . (1 . (2 . NIL))) => (cons + (cons 1 (cons 2 nil)))
 	// terminal nils are not represented but general shape is as expected
 	Cons(Box<Sexpr>, Box<Sexpr>),
-	Nil,
 }
-
-// TODO implement iterator
 
 impl Sexpr {
 	pub fn from_pair(pair: Pair<Rule>) -> Sexpr {
 		match pair.as_rule() {
-			Rule::nil => Sexpr::Nil,
-			Rule::float => parse_float(pair.as_str()),
-			Rule::int => parse_int(pair.as_str()),
-			Rule::boolean => parse_bool(pair.as_str()),
-			Rule::string => parse_string(pair.as_str()),
-			Rule::symbol => parse_symbol(pair.as_str()),
-			Rule::identifier => Sexpr::Identifier(String::from(pair.as_str())),
+			Rule::atom => parse_atom(pair),
 			Rule::list => {
 				let mut inner = pair.into_inner();
-				let left = Sexpr::from_pair(inner.next().unwrap());
-				let next = inner.next();
 
-				if next.is_none() {
-					Sexpr::cons(left, Sexpr::Nil)
-				} else {
-					let next_sexpr = Sexpr::from_pair(next.unwrap());
+				let car = Sexpr::from_pair(inner.next().unwrap());
+				let cdr = Sexpr::from_pairs(&mut inner);
 
-					let mut cdr = inner.map(Sexpr::from_pair).collect::<Vec<Sexpr>>();
-					cdr.insert(0, next_sexpr);
-
-					let mut base = Sexpr::Nil;
-
-					for sexpr in cdr.into_iter().rev() {
-						base = sexpr.cons_with(base);
-					}
-
-					left.cons_with(base)
-				}
+				Sexpr::cons(car, cdr)
 			}
 			_ => unreachable!(),
+		}
+	}
+
+	fn from_pairs(pairs: &mut Pairs<Rule>) -> Sexpr {
+		let next = pairs.next();
+
+		if next.is_none() {
+			Sexpr::Atom(Atom::Nil)
+		} else {
+			Sexpr::cons(Sexpr::from_pair(next.unwrap()), Sexpr::from_pairs(pairs))
 		}
 	}
 
@@ -87,9 +70,9 @@ impl Sexpr {
 		}
 	}
 
-	pub fn into_iter(&self) -> SexprIter {
-		SexprIter::new(self.clone())
-	}
+	// pub fn into_iter(&self) -> SexprIter {
+	// 	SexprIter::new(self.clone())
+	// }
 
 	fn cons_with(&self, other: Sexpr) -> Sexpr {
 		Sexpr::cons(self.clone(), other)
@@ -100,36 +83,51 @@ impl Sexpr {
 	}
 }
 
-impl From<&str> for Sexpr {
-	fn from(item: &str) -> Self {
-		Sexpr::String(String::from(item))
-	}
+fn parse_atom(pair: Pair<Rule>) -> Sexpr {
+	let pair_str = pair.as_str();
+
+	let atom = match pair.into_inner().next().unwrap().as_rule() {
+		Rule::nil => Atom::Nil,
+		Rule::float => parse_float(pair_str),
+		Rule::int => parse_int(pair_str),
+		Rule::boolean => parse_bool(pair_str),
+		Rule::string => parse_string(pair_str),
+		Rule::symbol => parse_symbol(pair_str),
+		Rule::identifier => parse_identifier(pair_str),
+		_ => unreachable!(),
+	};
+
+	Sexpr::Atom(atom)
 }
 
-fn parse_float(s: &str) -> Sexpr {
+fn parse_float(s: &str) -> Atom {
 	let parsed = s.parse::<f64>().unwrap();
-	Sexpr::Float(parsed)
+	Atom::Float(parsed)
 }
 
-fn parse_int(s: &str) -> Sexpr {
+fn parse_int(s: &str) -> Atom {
 	let parsed = s.parse::<i64>().unwrap();
-	Sexpr::Integer(parsed)
+	Atom::Integer(parsed)
 }
 
-fn parse_bool(s: &str) -> Sexpr {
+fn parse_bool(s: &str) -> Atom {
 	match s {
-		"true" => Sexpr::Boolean(true),
-		"false" => Sexpr::Boolean(false),
+		"true" => Atom::Boolean(true),
+		"false" => Atom::Boolean(false),
 		_ => unreachable!(), // hopefully!
 	}
 }
 
-fn parse_string(s: &str) -> Sexpr {
-	Sexpr::String(String::from(s))
+fn parse_string(s: &str) -> Atom {
+	Atom::String(String::from(s))
 }
 
-fn parse_symbol(s: &str) -> Sexpr {
-	Sexpr::Symbol(String::from(s))
+fn parse_symbol(s: &str) -> Atom {
+	Atom::Symbol(String::from(s))
+}
+
+fn parse_identifier(s: &str) -> Atom {
+	Atom::Identifier(String::from(s))
 }
 
 pub struct SexprIter {
@@ -179,26 +177,36 @@ mod sexpr_tests {
 		let parsed = DialParser::parse(Rule::atom, "2").unwrap().next().unwrap();
 		let result = Sexpr::from_pair(parsed);
 
-		assert_eq!(result, Sexpr::Integer(2));
+		assert_eq!(result, Sexpr::Atom(Atom::Integer(2)));
 	}
 
 	#[test]
 	fn from_list() {
 		let parsed = DialParser::parse(Rule::list, "(* 2 (+ 3 4 5))").unwrap();
 
+		let mul = Atom::Symbol(String::from("*"));
+		let two = Atom::Integer(2);
+		let plus = Atom::Symbol(String::from("+"));
+		let three = Atom::Integer(3);
+		let four = Atom::Integer(4);
+		let five = Atom::Integer(5);
+
 		let root = Sexpr::cons(
-			Sexpr::Symbol(String::from("*")),
+			Sexpr::Atom(mul),
 			Sexpr::cons(
-				Sexpr::Integer(2),
+				Sexpr::Atom(two),
 				Sexpr::cons(
 					Sexpr::cons(
-						Sexpr::Symbol(String::from("+")),
+						Sexpr::Atom(plus),
 						Sexpr::cons(
-							Sexpr::Integer(3),
-							Sexpr::cons(Sexpr::Integer(4), Sexpr::Integer(5)),
+							Sexpr::Atom(three),
+							Sexpr::cons(
+								Sexpr::Atom(four),
+								Sexpr::cons(Sexpr::Atom(five), Sexpr::Atom(Atom::Nil)),
+							),
 						),
 					),
-					Sexpr::Nil,
+					Sexpr::Atom(Atom::Nil),
 				),
 			),
 		);
@@ -212,16 +220,17 @@ mod sexpr_tests {
 	fn two_lists() {
 		let parsed = DialParser::parse(Rule::list, "((1 2) (3 4))").unwrap();
 
-		let root = Sexpr::cons(
-			Sexpr::cons(
-				Sexpr::Integer(1),
-				Sexpr::cons(Sexpr::Integer(2), Sexpr::Nil),
-			),
-			Sexpr::cons(
-				Sexpr::Integer(3),
-				Sexpr::cons(Sexpr::Integer(4), Sexpr::Nil),
-			),
+		let left = Sexpr::cons(
+			Sexpr::Atom(Atom::Integer(1)),
+			Sexpr::cons(Sexpr::Atom(Atom::Integer(2)), Sexpr::Atom(Atom::Nil)),
 		);
+
+		let right = Sexpr::cons(
+			Sexpr::Atom(Atom::Integer(3)),
+			Sexpr::cons(Sexpr::Atom(Atom::Integer(4)), Sexpr::Atom(Atom::Nil)),
+		);
+
+		let root = Sexpr::cons(left, Sexpr::cons(right, Sexpr::Atom(Atom::Nil)));
 
 		let mut sexprs = parsed.map(Sexpr::from_pair);
 
@@ -233,14 +242,14 @@ mod sexpr_tests {
 		let parsed = DialParser::parse(Rule::list, "(1 2 3 4 5)").unwrap();
 
 		let root = Sexpr::cons(
-			Sexpr::Integer(1),
+			Sexpr::Atom(Atom::Integer(1)),
 			Sexpr::cons(
-				Sexpr::Integer(2),
+				Sexpr::Atom(Atom::Integer(2)),
 				Sexpr::cons(
-					Sexpr::Integer(3),
+					Sexpr::Atom(Atom::Integer(3)),
 					Sexpr::cons(
-						Sexpr::Integer(4),
-						Sexpr::cons(Sexpr::Integer(5), Sexpr::Nil),
+						Sexpr::Atom(Atom::Integer(4)),
+						Sexpr::cons(Sexpr::Atom(Atom::Integer(5)), Sexpr::Atom(Atom::Nil)),
 					),
 				),
 			),
@@ -252,24 +261,24 @@ mod sexpr_tests {
 	}
 }
 
-#[cfg(test)]
-mod sexpriter_tests {
-	use super::*;
-	use pest::Parser;
+// #[cfg(test)]
+// mod sexpriter_tests {
+// 	use super::*;
+// 	use pest::Parser;
 
-	#[test]
-	fn iter_through_sexpr() {
-		let mut parsed = DialParser::parse(Rule::list, "(* 2 (+ 3 4 5))").unwrap();
-		let mut sexprs = Sexpr::from_pair(parsed.next().unwrap()).into_iter();
+// 	#[test]
+// 	fn iter_through_sexpr() {
+// 		let mut parsed = DialParser::parse(Rule::list, "(* 2 (+ 3 4 5))").unwrap();
+// 		let mut sexprs = Sexpr::from_pair(parsed.next().unwrap()).into_iter();
 
-		assert_eq!(sexprs.next().unwrap(), Sexpr::Symbol(String::from("*")));
-		assert_eq!(sexprs.next().unwrap(), Sexpr::Integer(2));
-		assert_eq!(sexprs.next().unwrap(), Sexpr::Symbol(String::from("+")));
-		assert_eq!(sexprs.next().unwrap(), Sexpr::Integer(3));
-		assert_eq!(sexprs.next().unwrap(), Sexpr::Integer(4));
-		assert_eq!(sexprs.next().unwrap(), Sexpr::Integer(5));
-		assert_eq!(sexprs.next().unwrap(), Sexpr::Nil);
-		assert_eq!(sexprs.next().unwrap(), Sexpr::Nil);
-		assert_eq!(sexprs.next(), None);
-	}
-}
+// 		assert_eq!(sexprs.next().unwrap(), Sexpr::Symbol(String::from("*")));
+// 		assert_eq!(sexprs.next().unwrap(), Sexpr::Integer(2));
+// 		assert_eq!(sexprs.next().unwrap(), Sexpr::Symbol(String::from("+")));
+// 		assert_eq!(sexprs.next().unwrap(), Sexpr::Integer(3));
+// 		assert_eq!(sexprs.next().unwrap(), Sexpr::Integer(4));
+// 		assert_eq!(sexprs.next().unwrap(), Sexpr::Integer(5));
+// 		assert_eq!(sexprs.next().unwrap(), Sexpr::Nil);
+// 		assert_eq!(sexprs.next().unwrap(), Sexpr::Nil);
+// 		assert_eq!(sexprs.next(), None);
+// 	}
+// }
