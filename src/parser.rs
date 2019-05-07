e7044b3b1d73c7bb60869b36718835db319dffaf
Ryan Faulhaber <faulhaberryan@gmail.com>
2019-05-06 20:54:52 -0400

    new ast finally done

--- src/parser.rs
+++ src/parser.rs
@@ -1,5 +1,6 @@
+use pest::error::LineColLocation;
 use pest::iterators::{Pair, Pairs};
-use std::collections::VecDeque;
+use pest::Parser;
 use std::fmt;
 
 #[derive(Parser)]
@@ -8,10 +9,10 @@ pub struct DialParser;
 
 // TODO make some macros!
 // TODO implement custom parsing error, returning useful values
-pub struct ParseError;
+pub type ParseResult = Result<Expr, String>;
 
 #[derive(Debug, PartialEq, Clone)]
-pub enum Atom {
+pub enum Expr {
 	Integer(i64),
 	Float(f64),
 	Boolean(bool),
@@ -19,381 +20,211 @@ pub enum Atom {
 	Symbol(String),
 	Identifier(String),
 	Nil,
+	List(Vec<Expr>),
 }
 
-impl fmt::Display for Atom {
+impl fmt::Display for Expr {
 	fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
 		match self {
-			Atom::Nil => write!(f, "nil"),
-			Atom::Integer(i) => write!(f, "{}", i),
-			Atom::Float(fl) => write!(f, "{}", fl),
-			Atom::Boolean(b) => write!(f, "{}", b),
-			Atom::String(s) => write!(f, "{}", s),
-			Atom::Symbol(s) => write!(f, "{}", s),
-			Atom::Identifier(s) => write!(f, "{}", s),
-		}
-	}
-}
+			Expr::Integer(i) => write!(f, "{}", i),
+			Expr::Float(fl) => write!(f, "{}", fl),
+			Expr::Boolean(b) => write!(f, "{}", b),
+			Expr::String(s) => write!(f, "{}", s),
+			Expr::Symbol(s) => write!(f, "{}", s),
+			Expr::Identifier(s) => write!(f, "{}", s),
+			Expr::Nil => write!(f, "nil"),
+			Expr::List(l) => {
+				let mut out_str = String::new();
 
-// intermediate representation of expressions
-#[derive(Debug, PartialEq, Clone)]
-pub enum Sexpr {
-	Atom(Atom),
-
-	// (+ 1 2) => (+ . (1 . (2 . NIL))) => (cons + (cons 1 (cons 2 nil)))
-	// terminal nils are not represented but general shape is as expected
-	Cons(Box<Sexpr>, Box<Sexpr>),
-}
-
-impl fmt::Display for Sexpr {
-	fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
-		match self {
-			Sexpr::Atom(a) => write!(f, "{}", a),
-			Sexpr::Cons(_, _) => {
-				println!("displaying a cons");
-
-				let mut out = String::new();
-
-				match self.car() {
-					Sexpr::Atom(a) => match a {
-						Atom::Nil => out.push(')'),
-						Atom::Symbol(s) => out.push_str(format!(" ({}", s).as_str()),
-						_ => out.push_str(format!(" {}", a).as_str()),
-					},
-					_ => out.push_str(format!("{}", self.car()).as_str()),
-				};
-
-				match self.cdr() {
-					Sexpr::Atom(a) => match a {
-						Atom::Nil => out.push(')'),
-						Atom::Symbol(s) => out.push_str(format!(" ({} ", s).as_str()),
-						_ => out.push_str(format!(" {}", a).as_str()),
-					},
-					_ => out.push_str(format!("{}", self.cdr()).as_str()),
+				for expr in l {
+					out_str.push_str(format!("{} ", expr).as_str());
 				}
 
-				write!(f, "{}", out)
+				out_str.pop();
+
+				write!(f, "({})", out_str)
 			}
 		}
 	}
 }
 
-impl Sexpr {
-	pub fn from_pair(pair: Pair<Rule>) -> Sexpr {
-		match pair.as_rule() {
-			Rule::atom => parse_atom(pair),
-			Rule::list => {
-				let mut inner = pair.into_inner();
+impl Expr {
+	pub fn from_list(s: &str) -> ParseResult {
+		let ast = DialParser::parse(Rule::list, s);
 
-				let car = Sexpr::from_pair(inner.next().unwrap());
-				let cdr = Sexpr::from_pairs(&mut inner);
+		match ast {
+			Ok(mut result) => Ok(parse_pair(result.next().unwrap())),
+			Err(err) => match err.line_col {
+				LineColLocation::Pos((line, col)) => {
+					let err_str = format!("error ({}, {}): invalid symbol", line, col);
 
-				Sexpr::cons(car, cdr)
+					Err(err_str)
+				}
+				LineColLocation::Span((line, col), (end_line, end_col)) => Err(format!(
+					"error ({}, {}), ({}, {}): invalid form",
+					line, col, end_line, end_col
+				)),
+			},
+		}
+	}
+
+	pub fn from_atom(s: &str) -> ParseResult {
+		let ast = DialParser::parse(Rule::atom, s);
+
+		match ast {
+			Ok(mut result) => Ok(parse_atom(result.next().unwrap())),
+			Err(err) => Err(format!("could not parse atom: {}", err)),
+		}
+	}
+
+	pub fn is_list(&self) -> bool {
+		match self {
+			Expr::List(_) => true,
+			_ => false,
+		}
+	}
+
+	pub fn into_iter(&self) -> ExprIter {
+		match self {
+			Expr::List(l) => ExprIter {
+				items: l.clone(),
+				current: 0,
+			},
+			_ => ExprIter {
+				items: vec![self.clone()],
+				current: 0,
+			},
+		}
+	}
+}
+
+fn parse_pair(pair: Pair<Rule>) -> Expr {
+	match pair.as_rule() {
+		Rule::atom => parse_atom(pair),
+		Rule::list => {
+			let mut inner = pair.into_inner();
+
+			Expr::List(inner.map(parse_pair).collect())
+		}
+		_ => unreachable!(),
+	}
+}
+
+fn parse_atom(pair: Pair<Rule>) -> Expr {
+	// atoms should only be one thing, so this is okay
+	let item = pair.into_inner().next().unwrap();
+
+	match item.as_rule() {
+		Rule::nil => Expr::Nil,
+		Rule::int => Expr::Integer(item.as_str().parse::<i64>().unwrap()),
+		Rule::float => Expr::Float(item.as_str().parse::<f64>().unwrap()),
+		Rule::boolean => {
+			match item.as_str() {
+				"true" => Expr::Boolean(true),
+				_ => Expr::Boolean(false), // hopefully this is correct!
+			}
+		}
+		Rule::string => Expr::String(String::from(item.as_str())),
+		Rule::symbol => Expr::Symbol(String::from(item.as_str())),
+		Rule::identifier => Expr::Identifier(String::from(item.as_str())),
+		_ => unreachable!(),
+	}
+}
+
+// TODO implement custom iterator where, if atom, returns self
+// otherwise, returns every expr in list
+pub struct ExprIter {
+	items: Vec<Expr>,
+	current: usize,
+}
+
+impl Iterator for ExprIter {
+	type Item = Expr;
+
+	fn next(&mut self) -> Option<Self::Item> {
+		let current = self.items.get(self.current);
+
+		match current {
+			Some(item) => {
+				self.current += 1;
+				Some(item.clone())
+			}
+			None => None,
+		}
+	}
+}
+
+#[cfg(test)]
+mod parser_test {
+	use super::*;
+
+	#[test]
+	fn display_expr() {
+		let expected = "(* 2 (+ 3 4 5))";
+		let expr = Expr::from_list(expected).unwrap();
+		let result = format!("{}", expr);
+
+		assert_eq!(expected, result);
+	}
+
+	#[test]
+	fn from_list_returns_expr() {
+		let parse_result = Expr::from_list("(* 2 (+ 3 4 5))");
+
+		assert!(parse_result.is_ok());
+
+		let result = parse_result.unwrap();
+
+		assert!(result.is_list());
+
+		match result {
+			Expr::List(l) => {
+				assert_eq!(l[0], Expr::Symbol(String::from("*")));
+				assert_eq!(l[1], Expr::Integer(2));
+				assert_eq!(
+					l[2],
+					Expr::List(vec![
+						Expr::Symbol(String::from("+")),
+						Expr::Integer(3),
+						Expr::Integer(4),
+						Expr::Integer(5),
+					])
+				);
 			}
 			_ => unreachable!(),
 		}
 	}
 
-	fn from_pairs(pairs: &mut Pairs<Rule>) -> Sexpr {
-		let next = pairs.next();
+	#[test]
+	fn from_atom_returns_expr() {
+		let parse_result = Expr::from_atom("2");
+		assert!(parse_result.is_ok());
 
-		if next.is_none() {
-			Sexpr::Atom(Atom::Nil)
-		} else {
-			Sexpr::cons(Sexpr::from_pair(next.unwrap()), Sexpr::from_pairs(pairs))
-		}
-	}
-
-	pub fn car(&self) -> Sexpr {
-		match self {
-			Sexpr::Cons(left, _) => *left.clone(),
-			_ => self.clone(),
-		}
-	}
-
-	pub fn cdr(&self) -> Sexpr {
-		match self {
-			Sexpr::Cons(_, right) => *right.clone(),
-			_ => self.clone(),
-		}
-	}
-
-	pub fn is_symbol(&self) -> bool {
-		match self {
-			Sexpr::Atom(a) => match a {
-				Atom::Symbol(_) => true,
-				_ => false,
-			},
-			_ => false,
-		}
-	}
-
-	pub fn is_cons(&self) -> bool {
-		match self {
-			Sexpr::Cons(left, right) => true,
-			_ => false,
-		}
-	}
-
-	pub fn is_atom(&self) -> bool {
-		match self {
-			Sexpr::Atom(_) => true,
-			_ => false,
-		}
-	}
-
-	pub fn is_identifier(&self) -> bool {
-		match self {
-			Sexpr::Atom(a) => match a {
-				Atom::Identifier(_) => true,
-				_ => false,
-			},
-			_ => false,
-		}
-	}
-
-	fn into_iter(&self) -> SexprIter {
-		SexprIter::new(self.clone())
-	}
-
-	fn cons_with(&self, other: Sexpr) -> Sexpr {
-		Sexpr::cons(self.clone(), other)
-	}
-
-	fn cons(left: Sexpr, right: Sexpr) -> Sexpr {
-		Sexpr::Cons(Box::new(left), Box::new(right))
-	}
-}
-
-fn parse_atom(pair: Pair<Rule>) -> Sexpr {
-	let pair_str = pair.as_str();
-
-	let atom = match pair.into_inner().next().unwrap().as_rule() {
-		Rule::nil => Atom::Nil,
-		Rule::float => parse_float(pair_str),
-		Rule::int => parse_int(pair_str),
-		Rule::boolean => parse_bool(pair_str),
-		Rule::string => parse_string(pair_str),
-		Rule::symbol => parse_symbol(pair_str),
-		Rule::identifier => parse_identifier(pair_str),
-		_ => unreachable!(),
-	};
-
-	Sexpr::Atom(atom)
-}
-
-fn parse_float(s: &str) -> Atom {
-	let parsed = s.parse::<f64>().unwrap();
-	Atom::Float(parsed)
-}
-
-fn parse_int(s: &str) -> Atom {
-	let parsed = s.parse::<i64>().unwrap();
-	Atom::Integer(parsed)
-}
-
-fn parse_bool(s: &str) -> Atom {
-	match s {
-		"true" => Atom::Boolean(true),
-		"false" => Atom::Boolean(false),
-		_ => unreachable!(), // hopefully!
-	}
-}
-
-fn parse_string(s: &str) -> Atom {
-	Atom::String(String::from(s))
-}
-
-fn parse_symbol(s: &str) -> Atom {
-	Atom::Symbol(String::from(s))
-}
-
-fn parse_identifier(s: &str) -> Atom {
-	Atom::Identifier(String::from(s))
-}
-
-pub struct SexprIter {
-	queue: VecDeque<Atom>,
-}
-
-impl SexprIter {
-	fn new(expr: Sexpr) -> Self {
-		let queue = SexprIter::preorder(expr)
-			.into_iter()
-			.collect::<VecDeque<Atom>>();
-
-		SexprIter { queue }
-	}
-
-	fn preorder(expr: Sexpr) -> Vec<Atom> {
-		match expr {
-			Sexpr::Cons(left, right) => {
-				let mut left_tree = SexprIter::preorder(*left.clone());
-				let mut right_tree = SexprIter::preorder(*right.clone());
-
-				left_tree.append(&mut right_tree);
-
-				left_tree
-			}
-			Sexpr::Atom(a) => vec![a],
-		}
-	}
-}
-
-impl Iterator for SexprIter {
-	type Item = Atom;
-
-	fn next(&mut self) -> Option<Self::Item> {
-		self.queue.pop_front()
+		assert_eq!(parse_result.unwrap(), Expr::Integer(2));
 	}
 }
 
 #[cfg(test)]
-mod sexpr_tests {
+mod expr_iter_test {
 	use super::*;
-	use pest::Parser;
 
 	#[test]
-	fn from_atom() {
-		let parsed = DialParser::parse(Rule::atom, "2").unwrap().next().unwrap();
-		let result = Sexpr::from_pair(parsed);
+	fn iter_returns_correctly() {
+		let parse_result = Expr::from_list("(* 2 (+ 3 4 5))");
+		let mut iter = parse_result.unwrap().into_iter();
 
-		assert_eq!(result, Sexpr::Atom(Atom::Integer(2)));
-	}
-
-	#[test]
-	fn from_list() {
-		let parsed = DialParser::parse(Rule::list, "(* 2 (+ 3 4 5))").unwrap();
-
-		let mul = Atom::Symbol(String::from("*"));
-		let two = Atom::Integer(2);
-		let plus = Atom::Symbol(String::from("+"));
-		let three = Atom::Integer(3);
-		let four = Atom::Integer(4);
-		let five = Atom::Integer(5);
-
-		let root = Sexpr::cons(
-			Sexpr::Atom(mul),
-			Sexpr::cons(
-				Sexpr::Atom(two),
-				Sexpr::cons(
-					Sexpr::cons(
-						Sexpr::Atom(plus),
-						Sexpr::cons(
-							Sexpr::Atom(three),
-							Sexpr::cons(
-								Sexpr::Atom(four),
-								Sexpr::cons(Sexpr::Atom(five), Sexpr::Atom(Atom::Nil)),
-							),
-						),
-					),
-					Sexpr::Atom(Atom::Nil),
-				),
-			),
+		assert_eq!(iter.next().unwrap(), Expr::Symbol(String::from("*")));
+		assert_eq!(iter.next().unwrap(), Expr::Integer(2));
+		assert_eq!(
+			iter.next().unwrap(),
+			Expr::List(vec![
+				Expr::Symbol(String::from("+")),
+				Expr::Integer(3),
+				Expr::Integer(4),
+				Expr::Integer(5),
+			])
 		);
 
-		let mut sexprs = parsed.map(Sexpr::from_pair);
-
-		assert_eq!(sexprs.next().unwrap(), root);
-	}
-
-	#[test]
-	fn two_lists() {
-		let parsed = DialParser::parse(Rule::list, "((1 2) (3 4))").unwrap();
-
-		let left = Sexpr::cons(
-			Sexpr::Atom(Atom::Integer(1)),
-			Sexpr::cons(Sexpr::Atom(Atom::Integer(2)), Sexpr::Atom(Atom::Nil)),
-		);
-
-		let right = Sexpr::cons(
-			Sexpr::Atom(Atom::Integer(3)),
-			Sexpr::cons(Sexpr::Atom(Atom::Integer(4)), Sexpr::Atom(Atom::Nil)),
-		);
-
-		let root = Sexpr::cons(left, Sexpr::cons(right, Sexpr::Atom(Atom::Nil)));
-
-		let mut sexprs = parsed.map(Sexpr::from_pair);
-
-		assert_eq!(sexprs.next().unwrap(), root);
-	}
-
-	#[test]
-	fn flat_list() {
-		let parsed = DialParser::parse(Rule::list, "(1 2 3 4 5)").unwrap();
-
-		let root = Sexpr::cons(
-			Sexpr::Atom(Atom::Integer(1)),
-			Sexpr::cons(
-				Sexpr::Atom(Atom::Integer(2)),
-				Sexpr::cons(
-					Sexpr::Atom(Atom::Integer(3)),
-					Sexpr::cons(
-						Sexpr::Atom(Atom::Integer(4)),
-						Sexpr::cons(Sexpr::Atom(Atom::Integer(5)), Sexpr::Atom(Atom::Nil)),
-					),
-				),
-			),
-		);
-
-		let mut sexprs = parsed.map(Sexpr::from_pair);
-
-		assert_eq!(sexprs.next().unwrap(), root);
-	}
-
-	#[test]
-	fn sexpr_display() {
-		let mul = Atom::Symbol(String::from("*"));
-		let two = Atom::Integer(2);
-		let plus = Atom::Symbol(String::from("+"));
-		let three = Atom::Integer(3);
-		let four = Atom::Integer(4);
-		let five = Atom::Integer(5);
-
-		let root = Sexpr::cons(
-			Sexpr::Atom(mul),
-			Sexpr::cons(
-				Sexpr::Atom(two),
-				Sexpr::cons(
-					Sexpr::cons(
-						Sexpr::Atom(plus),
-						Sexpr::cons(
-							Sexpr::Atom(three),
-							Sexpr::cons(
-								Sexpr::Atom(four),
-								Sexpr::cons(Sexpr::Atom(five), Sexpr::Atom(Atom::Nil)),
-							),
-						),
-					),
-					Sexpr::Atom(Atom::Nil),
-				),
-			),
-		);
-
-		let result = format!("{}", root);
-
-		assert_eq!("(* 2 (+ 3 4 5))", result.as_str());
-	}
-}
-
-#[cfg(test)]
-mod sexpriter_tests {
-	use super::*;
-	use pest::Parser;
-
-	#[test]
-	fn iter_through_sexpr() {
-		let mut parsed = DialParser::parse(Rule::list, "(* 2 (+ 3 4 5))").unwrap();
-		let mut sexprs = Sexpr::from_pair(parsed.next().unwrap()).into_iter();
-
-		assert_eq!(sexprs.next().unwrap(), Atom::Symbol(String::from("*")));
-		assert_eq!(sexprs.next().unwrap(), Atom::Integer(2));
-		assert_eq!(sexprs.next().unwrap(), Atom::Symbol(String::from("+")));
-		assert_eq!(sexprs.next().unwrap(), Atom::Integer(3));
-		assert_eq!(sexprs.next().unwrap(), Atom::Integer(4));
-		assert_eq!(sexprs.next().unwrap(), Atom::Integer(5));
-		assert_eq!(sexprs.next().unwrap(), Atom::Nil);
-		assert_eq!(sexprs.next().unwrap(), Atom::Nil);
-		assert_eq!(sexprs.next(), None);
+		assert_eq!(iter.next(), None);
 	}
 }
