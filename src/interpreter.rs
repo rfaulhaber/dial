abe4afaaee342dadaf44d7dacf2647d50244408a
Ryan Faulhaber <faulhaberryan@gmail.com>
2019-06-03 20:55:44 -0400

    step 5 implemented

--- src/interpreter.rs
+++ src/interpreter.rs
@@ -9,209 +9,223 @@ pub type EvalResult = Result<Expr, String>;
 
 pub struct Interpreter {
     env: Env,
+    close_env: bool,
 }
 
 impl Interpreter {
     pub fn new() -> Interpreter {
         Interpreter {
             env: Env::default(),
+            close_env: false,
         }
     }
 
-    // TODO please god make this method shorter
+    // TODO please god make this method shorter and fix this right drift
     pub fn eval(&mut self, expr: Expr) -> EvalResult {
-        match expr {
-            Expr::Atom(atom) => match atom {
-                Atom::Symbol(symbol) => {
-                    let lookup = self.env.get(&symbol);
+        let mut inner_expr = expr;
+        let result = loop {
+            match inner_expr {
+                Expr::Atom(atom) => match atom {
+                    Atom::Symbol(symbol) => {
+                        let lookup = self.env.get(&symbol);
 
-                    match lookup {
-                        Some(result) => Ok(result),
-                        None => Err(format!("cannot resolve symbol: {}", symbol)),
+                        break match lookup {
+                            Some(result) => Ok(result),
+                            None => Err(format!("cannot resolve symbol: {}", symbol)),
+                        };
                     }
-                }
-                Atom::Identifier(id) => {
-                    let lookup = self.env.get(&id);
+                    Atom::Identifier(id) => {
+                        let lookup = self.env.get(&id);
 
-                    match lookup {
-                        Some(result) => Ok(result),
-                        None => Err(format!("cannot resolve symbol: {}", id)),
+                        break match lookup {
+                            Some(result) => Ok(result),
+                            None => Err(format!("cannot resolve symbol: {}", id)),
+                        };
                     }
+                    _ => break Ok(atom.into()),
+                },
+                Expr::Vector(vec) => {
+                    if vec.is_empty() {
+                        return Ok(Expr::Vector(Vec::new()));
+                    }
+
+                    let vec_eval: Result<Vec<Expr>, String> =
+                        vec.iter().map(|expr| self.eval(expr.clone())).collect();
+
+                    break match vec_eval {
+                        Ok(exprs) => Ok(Expr::Vector(exprs)),
+                        Err(err) => Err(err),
+                    };
                 }
-                _ => Ok(atom.into()),
-            },
-            Expr::Vector(vec) => {
-                if vec.is_empty() {
-                    return Ok(Expr::Vector(Vec::new()));
-                }
+                Expr::List(list) => {
+                    if list.is_empty() {
+                        return Ok(Expr::List(Vec::new()));
+                    }
 
-                let vec_eval: Result<Vec<Expr>, String> =
-                    vec.iter().map(|expr| self.eval(expr.clone())).collect();
+                    let first = &list[0];
 
-                match vec_eval {
-                    Ok(exprs) => Ok(Expr::Vector(exprs)),
-                    Err(err) => Err(err),
-                }
-            }
-            Expr::List(list) => {
-                if list.is_empty() {
-                    return Ok(Expr::List(Vec::new()));
-                }
-
-                let first = &list[0];
-
-                // TODO add validation!!!
-                if is_list_special(first) {
-                    match get_list_special(first).as_str() {
-                        "if" => {
-                            if list[1..].len() > 3 {
-                                return Err("too many arguments for if".to_string());
-                            }
-
-                            let cond = self.eval(list[1].clone())?;
-
-                            if is_false_or_nil(&cond) {
-                                match list.get(3) {
-                                    Some(expr) => self.eval(expr.clone()),
-                                    None => Ok(Expr::Atom(Atom::Nil)),
+                    // TODO add validation!!!
+                    if is_list_special(first) {
+                        match get_list_special(first).as_str() {
+                            "if" => {
+                                if list[1..].len() > 3 {
+                                    return Err("too many arguments for if".to_string());
                                 }
-                            } else {
-                                self.eval(list[2].clone())
-                            }
-                        }
-                        "do" => {
-                            let args = &list[1..];
 
-                            args.iter()
-                                .map(|expr| self.eval(expr.clone()))
-                                .last()
-                                .unwrap()
-                        }
-                        "def" => {
-                            let first_atom = &list[1].as_atom();
-                            let symbol = match first_atom {
-                                Some(atom) => match atom {
-                                    Atom::Identifier(id) => id,
-                                    _ => {
-                                        return Err(
-                                            "first argument of def must be valid identifier"
-                                                .to_string(),
-                                        )
+                                let cond = self.eval(list[1].clone())?;
+
+                                if is_false_or_nil(&cond) {
+                                    inner_expr = match list.get(3) {
+                                        Some(expr) => expr.clone(),
+                                        None => Atom::Nil.into(),
                                     }
-                                },
-                                None => {
-                                    return Err("first argument of def must be valid identifier"
-                                        .to_string())
+                                } else {
+                                    inner_expr = list[2].clone();
                                 }
-                            };
-
-                            let def = self.eval(list[2].clone())?;
-
-                            self.env.set(symbol, def.clone());
-
-                            Ok(def)
-                        }
-                        "let" => {
-                            // TODO validate
-                            let bindings = match &list[1] {
-                                Expr::Vector(vec) => vec,
-                                _ => return Err("let bindings must be a vector".to_string()),
-                            };
-
-                            info!("bindings: {:?}", bindings);
-
-                            if bindings.len() % 2 != 0 {
-                                return Err("let bindings must be even".to_string());
                             }
+                            "do" => {
+                                let args = &list[1..list.len() - 1];
 
-                            info!("pushing scope");
+                                let previous: Result<Vec<Expr>, String> =
+                                    args.iter().map(|expr| self.eval(expr.clone())).collect();
 
-                            self.push_scope();
-
-                            let iter = BindingIterator::from(bindings.clone());
-
-                            for (symbol, value) in iter {
-                                info!("binding symbol {:?} to value {:?}", symbol, value);
-                                self.env.set(&symbol, value);
+                                match previous {
+                                    Err(message) => break Err(message),
+                                    _ => {
+                                        inner_expr = list[list.len() - 1].clone();
+                                        continue;
+                                    }
+                                }
                             }
+                            "def" => {
+                                let first_atom = &list[1].as_atom();
+                                let symbol =
+                                    match first_atom {
+                                        Some(atom) => match atom {
+                                            Atom::Identifier(id) => id,
+                                            _ => return Err(
+                                                "first argument of def must be valid identifier"
+                                                    .to_string(),
+                                            ),
+                                        },
+                                        None => {
+                                            return Err(
+                                                "first argument of def must be valid identifier"
+                                                    .to_string(),
+                                            )
+                                        }
+                                    };
 
-                            // TODO validate
-                            let body = list[2].clone();
+                                let def = self.eval(list[2].clone())?;
 
-                            let result = self.eval(body);
+                                self.env.set(symbol, def.clone());
 
-                            info!("popping scope");
-                            self.pop_scope();
-
-                            result
-                        }
-                        "fn" => {
-                            // TODO validation on list size
-                            let bindings_atoms: Result<Vec<Atom>, &'static str> =
-                                extract_vec_from_expr(list[1].clone())?
-                                    .iter()
-                                    .map(|expr| unwrap_atom_from_expr(expr.clone()))
-                                    .collect();
-
-                            let bindings_strings: Result<Vec<String>, &'static str> =
-                                bindings_atoms?
-                                    .iter()
-                                    .map(|atom| match atom {
-                                        Atom::Identifier(id) => Ok(id.clone()),
-                                        _ => Err("invalid identifier"),
-                                    })
-                                    .collect();
-
-                            let body = Box::new(list[2].clone());
-
-                            let lambda = Lambda::new(bindings_strings?, body);
-
-                            Ok(Expr::from(lambda))
-                        }
-                        _ => unreachable!(),
-                    }
-                } else {
-                    let first_result = self.eval(list[0].clone())?;
-
-                    let args = &list[1..];
-
-                    match first_result {
-                        Expr::Atom(a) => match a {
-                            Atom::Macro(f) => {
-                                let args_eval: Result<Vec<Expr>, String> = args
-                                    .iter()
-                                    .map(|arg_expr| self.eval(arg_expr.clone()))
-                                    .collect();
-
-                                f(&args_eval?)
+                                break Ok(def);
                             }
-                            Atom::Lambda(l) => {
-                                let Lambda { params, body } = l;
+                            "let" => {
+                                // TODO validate
+                                let bindings = match &list[1] {
+                                    Expr::Vector(vec) => vec,
+                                    _ => return Err("let bindings must be a vector".to_string()),
+                                };
 
-                                let args_eval: Result<Vec<Expr>, String> = args
-                                    .iter()
-                                    .map(|arg_expr| self.eval(arg_expr.clone()))
-                                    .collect();
+                                info!("bindings: {:?}", bindings);
+
+                                if bindings.len() % 2 != 0 {
+                                    return Err("let bindings must be even".to_string());
+                                }
+
+                                info!("pushing scope");
 
                                 self.push_scope();
 
-                                for (symbol, arg_eval) in params.iter().zip(args_eval?.iter()) {
-                                    self.env.set(&symbol, arg_eval.clone());
-                                }
+                                bindings.clone().chunks(2).for_each(|chunk| {
+                                    let symbol_expr = chunk[0].clone();
+                                    let value = chunk[1].clone();
+                                    self.env
+                                        .set(&extract_identifier_from_expr(symbol_expr), value)
+                                });
 
-                                let body_eval = self.eval(*body);
-
-                                self.pop_scope();
-
-                                body_eval
+                                // TODO validate
+                                inner_expr = list[2].clone();
+                                self.close_env = true;
+                                continue;
                             }
-                            _ => Err("invalid form".to_string()),
-                        },
-                        _ => Err("invalid form".to_string()),
+                            "fn" => {
+                                // TODO validation on list size
+                                let bindings_atoms: Result<Vec<Atom>, &'static str> =
+                                    extract_vec_from_expr(list[1].clone())?
+                                        .iter()
+                                        .map(|expr| unwrap_atom_from_expr(expr.clone()))
+                                        .collect();
+
+                                let bindings_strings: Result<Vec<String>, &'static str> =
+                                    bindings_atoms?
+                                        .iter()
+                                        .map(|atom| match atom {
+                                            Atom::Identifier(id) => Ok(id.clone()),
+                                            _ => Err("invalid identifier"),
+                                        })
+                                        .collect();
+
+                                let body = Box::new(list[2].clone());
+
+                                let lambda = Lambda::new(bindings_strings?, body);
+
+                                break Ok(Expr::from(lambda));
+                            }
+                            _ => unreachable!(),
+                        }
+                    } else {
+                        let first_result = self.eval(list[0].clone())?;
+
+                        let args = &list[1..];
+
+                        match first_result {
+                            Expr::Atom(a) => match a {
+                                Atom::Macro(f) => {
+                                    let args_eval: Result<Vec<Expr>, String> = args
+                                        .iter()
+                                        .map(|arg_expr| self.eval(arg_expr.clone()))
+                                        .collect();
+
+                                    break f(&args_eval?);
+                                }
+                                Atom::Lambda(l) => {
+                                    let Lambda { params, body } = l;
+
+                                    let args_eval: Result<Vec<Expr>, String> = args
+                                        .iter()
+                                        .map(|arg_expr| self.eval(arg_expr.clone()))
+                                        .collect();
+
+                                    self.push_scope();
+
+                                    for (symbol, arg_eval) in params.iter().zip(args_eval?.iter()) {
+                                        self.env.set(&symbol, arg_eval.clone());
+                                    }
+
+                                    let body_eval = self.eval(*body);
+
+                                    self.pop_scope();
+
+                                    break body_eval;
+                                }
+                                _ => break Err("invalid form".to_string()),
+                            },
+                            _ => break Err("invalid form".to_string()),
+                        }
                     }
                 }
             }
+        };
+
+        if self.close_env {
+            self.close_env = false;
+            self.pop_scope();
         }
+
+        result
     }
 
     fn push_scope(&mut self) {
@@ -271,6 +285,16 @@ fn extract_vec_from_expr(expr: Expr) -> Result<Vec<Expr>, &'static str> {
     match expr {
         Expr::Vector(vec) => Ok(vec),
         _ => Err("expr is not vector"),
+    }
+}
+
+fn extract_identifier_from_expr(expr: Expr) -> String {
+    match expr {
+        Expr::Atom(a) => match a {
+            Atom::Identifier(id) => id,
+            _ => unreachable!(),
+        },
+        _ => unreachable!(),
     }
 }
 
@@ -416,8 +440,49 @@ mod interpreter_test {
     }
 
     #[test]
-    fn let_scopes_value() {}
+    fn let_scopes_value() {
+        let parsed = DialParser::parse(Rule::list, "(do (let [c 2] c) (+ c 2))").unwrap();
+        let expr = Expr::from(parsed);
+
+        let mut int = Interpreter::new();
+        let result = int.eval(expr);
+
+        assert!(result.is_err());
+        assert_eq!(Err("cannot resolve symbol: c".to_string()), result);
+    }
 
     #[test]
-    fn let_overrides_value() {}
+    fn let_overrides_value() {
+        let parsed = DialParser::parse(Rule::list, "(do (def c 2) (let [c 3] c))").unwrap();
+        let expr = Expr::from(parsed);
+
+        let mut int = Interpreter::new();
+        let result = int.eval(expr);
+
+        assert!(result.is_ok());
+        assert_eq!(Ok(3.into()), result);
+    }
+
+    #[test]
+    fn fn_creates_closure() {
+        let parsed = DialParser::parse(Rule::list, "(fn [a b] (+ a b))").unwrap();
+        let expr = Expr::from(parsed);
+
+        let mut int = Interpreter::new();
+        let result = int.eval(expr);
+
+        assert!(result.is_ok());
+    }
+
+    #[test]
+    fn fn_creates_callable_closure() {
+        let parsed = DialParser::parse(Rule::list, "((fn [a b] (+ a b)) 2 3)").unwrap();
+        let expr = Expr::from(parsed);
+
+        let mut int = Interpreter::new();
+        let result = int.eval(expr);
+
+        assert!(result.is_ok());
+        assert_eq!(Ok(5.into()), result);
+    }
 }
