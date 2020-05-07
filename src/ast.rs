774cbdc9a65775e4726b386f169670dc907d973f
Ryan Faulhaber <faulhaberryan@gmail.com>
2020-05-07 16:13:06 -0400

    worked on symbol parsing

--- src/ast.rs
+++ src/ast.rs
@@ -1,7 +1,26 @@
+use std::fmt::Display;
+
 #[derive(Debug, PartialEq)]
-pub enum Sexpr<'s> {
+pub enum S<'s> {
 	Atom(Box<Atom<'s>>),
-	List(Vec<Sexpr<'s>>),
+	List(Vec<Atom<'s>>),
+}
+
+impl <'s> Display for S<'s> {
+	fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+		match self {
+			S::Atom(a) => write!(f, "{}", a),
+			S::List(l) => {
+				write!(f, "(")?;
+
+				for v in l {
+					write!(f, "{}", v)?;
+				}
+
+				Ok(write!(f, ")")?)
+			}
+		}
+	}
 }
 
 #[derive(Debug, PartialEq)]
@@ -31,5 +50,23 @@ impl<'a> From<f64> for Atom<'a> {
 impl <'a> From<bool> for Atom<'a> {
 	fn from(b: bool) -> Atom<'a> {
 		Atom::Bool(b)
+	}
+}
+
+impl<'a> Display for Atom<'a> {
+	fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+		match self {
+			Atom::Nil => write!(f, "nil"),
+			Atom::Bool(v)  => write!(f, "{}", v),
+			Atom::Int(v) => write!(f, "{}", v),
+			Atom::Float(v) => write!(f, "{}", v),
+			Atom::Sym(v) => write!(f, "{}", v),
+			Atom::Str(v) => write!(f, "\"{}\"", v),
+			Atom::Keyword(v) => write!(f, ":{}", v),
+			Atom::Vec(v) => {
+				let val_strs: Vec<String> = v.iter().map(|v| format!("{}", v)).collect();
+				write!(f, "[{}]", val_strs.join(", "))
+			}
+		}
 	}
 }
--- src/parse.rs
+++ src/parse.rs
@@ -1,10 +1,10 @@
 use super::ast::*;
 use nom::{
 	branch::alt,
-	bytes::complete::tag,
-	character::complete::digit1,
+	bytes::complete::{is_not, tag, take_until},
+	character::complete::{char, digit1},
 	combinator::{map, map_res},
-	sequence::{preceded, tuple},
+	sequence::{delimited, preceded, tuple},
 	IResult,
 };
 
@@ -13,7 +13,7 @@ pub type ParseResult<T> = Result<T, ParseError>;
 #[derive(Debug)]
 pub struct ParseError(String);
 
-pub fn parse_sexpr<'s>(input: &str) -> ParseResult<Sexpr<'s>> {
+pub fn parse_sexpr<'s>(input: &str) -> ParseResult<S<'s>> {
 	unimplemented!();
 }
 
@@ -21,26 +21,36 @@ pub fn parse_atom<'a>(input: &str) -> ParseResult<Atom<'a>> {
 	unimplemented!();
 }
 
-fn sexpr<'s>(input: &str) -> IResult<&str, Sexpr<'s>> {
+fn sexpr<'s>(input: &str) -> IResult<&str, S<'s>> {
 	unimplemented!();
 }
 
-fn atom<'a>(input: &str) -> IResult<&str, Atom<'a>> {
+fn atom(input: &str) -> IResult<&str, Atom<'_>> {
 	alt((
 		float_atom,
-		int_atom
+		int_atom,
+		str_atom,
+		keyword_atom,
 	))(input)
 }
 
-fn int_atom<'a>(input: &str) -> IResult<&str, Atom<'a>> {
+fn int_atom(input: &str) -> IResult<&str, Atom<'_>> {
 	map(int, |i: i64| Atom::Int(i))(input)
 }
 
-fn float_atom<'a>(input: &str) -> IResult<&str, Atom<'a>> {
+fn float_atom(input: &str) -> IResult<&str, Atom<'_>> {
 	map(float, |f: f64| Atom::Float(f))(input)
 }
 
-fn int<'a>(input: &str) -> IResult<&str, i64> {
+fn str_atom(input: &str) -> IResult<&str, Atom<'_>> {
+	map(str, |s| Atom::Str(s))(input)
+}
+
+fn keyword_atom(input: &str) -> IResult<&str, Atom<'_>> {
+	map(keyword, |s| Atom::Sym(s))(input)
+}
+
+fn int(input: &str) -> IResult<&str, i64> {
 	alt((
 		map_res(digit1, |digit_str: &str| digit_str.parse::<i64>()),
 		map_res(preceded(tag("-"), digit1), |digit_str: &str| {
@@ -49,7 +59,7 @@ fn int<'a>(input: &str) -> IResult<&str, i64> {
 	))(input)
 }
 
-fn float<'a>(input: &str) -> IResult<&str, f64> {
+fn float(input: &str) -> IResult<&str, f64> {
 	map_res(
 		tuple((int, tag("."), int)),
 		|(head, _, tail): (i64, &str, i64)| {
@@ -59,19 +69,36 @@ fn float<'a>(input: &str) -> IResult<&str, f64> {
 	)(input)
 }
 
+fn str(input: &str) -> IResult<&str, &str> {
+	preceded(tag("\""), take_until("\""))(input)
+}
+
+fn keyword(input: &str) -> IResult<&str, &str> {
+	preceded(tag(":"), take_until(" "))(input)
+}
+
+fn sym(input: &str) -> IResult<&str, &str> {
+	todo!("implement me!");
+}
+
 #[cfg(test)]
 mod tests {
 	use super::*;
 
 	#[test]
 	fn atom_test() {
-		let inputs = vec!["12", "-34.5"];
+		let inputs = vec!["12", "-34.5", r#""foo bar""#, ":foo"];
 		let res: Vec<Atom<'_>> = inputs.iter().map(|s| atom(s).unwrap().1).collect();
 
-		assert_eq!(res, vec![
-			Atom::Int(12),
-			Atom::Float(-34.5)
-		]);
+		assert_eq!(
+			res,
+			vec![
+				Atom::Int(12),
+				Atom::Float(-34.5),
+				Atom::Str("foo bar"),
+				Atom::Keyword("foo")
+			]
+		);
 	}
 
 	#[test]
@@ -79,21 +106,16 @@ mod tests {
 		let inputs = vec!["-123", "4", "0"];
 		let res: Vec<Atom<'_>> = inputs.iter().map(|s| int_atom(s).unwrap().1).collect();
 
-		assert_eq!(res, vec![
-			Atom::Int(-123),
-			Atom::Int(4),
-			Atom::Int(0)
-		]);
+		assert_eq!(res, vec![Atom::Int(-123), Atom::Int(4), Atom::Int(0)]);
 	}
 
 	#[test]
 	fn float_test() {
 		let inputs = vec!["0.123", "4.56", "-7.089"];
 		let res: Vec<Atom<'_>> = inputs.iter().map(|s| float_atom(s).unwrap().1).collect();
-		assert_eq!(res, vec![
-			Atom::Float(0.123),
-			Atom::Float(4.56),
-			Atom::Float(-7.89)
-		]);
+		assert_eq!(
+			res,
+			vec![Atom::Float(0.123), Atom::Float(4.56), Atom::Float(-7.89)]
+		);
 	}
 }
