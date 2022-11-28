07e97f01806b758fd1458c44c9463d5b5200bc32
Ryan Faulhaber <ryf@sent.as>
2022-11-28 17:59:58 -0500

    modernize boilerplate

--- flake.nix
+++ flake.nix
@@ -1,11 +1,39 @@
 {
+  description = "dial";
   inputs = {
-    nixpkgs.url = "github:nixos/nixpkgs";
+    nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
     flake-utils.url = "github:numtide/flake-utils";
   };
 
   outputs = { self, nixpkgs, flake-utils }:
     flake-utils.lib.eachDefaultSystem (system:
-      let pkgs = nixpkgs.legacyPackages.${system};
-      in { devShell = import ./shell.nix { inherit pkgs; }; });
+      let
+        pkgs = nixpkgs.legacyPackages.${system};
+        projectName = "dial";
+      in rec {
+        packages.${projectName} = pkgs.rustPlatform.buildRustPackage {
+          pname = projectName;
+          version = "0.1.0";
+          src = ./.;
+          cargoLock.lockFile = ./Cargo.lock;
+        };
+
+        packages.default = self.packages.${system}.${projectName};
+
+        apps.${projectName} =
+          flake-utils.lib.mkApp { drv = packages.${projectName}; };
+
+        apps.default = self.apps.${system}.${projectName};
+
+        devShells.default = pkgs.mkShell {
+          buildInputs = with pkgs; [
+            cargo
+            rustc
+            rustfmt
+            clippy
+            rust-analyzer
+            rustup
+          ];
+        };
+      });
 }
--- shell.nix
+++
@@ -1,7 +0,0 @@
-{ pkgs ? import <nixpkgs> { } }:
-
-with pkgs;
-
-mkShell {
-  buildInputs = [ cargo rustc rustfmt clippy rust-analyzer rls rustup ];
-}
